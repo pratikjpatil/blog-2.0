e4703cf94019133503a3e8bafd4a1f5ea40a322a
Pratik <pratikpatil8560@gmail.com>
2022-08-06 11:10:11 +0530

    initial commit

--- app.js
+++ app.js
@@ -4,35 +4,66 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const _ = require("lodash");
+const mongoose = require("mongoose");
 
-const homeStartingContent = "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
-const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
+
+const homeStartingContent = "";
 const contactContent = "Scelerisque eleifend donec pretium vulputate sapien. Rhoncus urna neque viverra justo nec ultrices. Arcu dui vivamus arcu felis bibendum. Consectetur adipiscing elit duis tristique. Risus viverra adipiscing at in tellus integer feugiat. Sapien nec sagittis aliquam malesuada bibendum arcu vitae. Consequat interdum varius sit amet mattis. Iaculis nunc sed augue lacus. Interdum posuere lorem ipsum dolor sit amet consectetur adipiscing elit. Pulvinar elementum integer enim neque. Ultrices gravida dictum fusce ut placerat orci nulla. Mauris in aliquam sem fringilla ut morbi tincidunt. Tortor posuere ac ut consequat semper viverra nam libero.";
-let posts = [];
+
 
 const app = express();
-app.use(bodyParser.urlencoded({
-  extended: true
-}));
+app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 app.set('view engine', 'ejs');
+const uri = 'mongodb://localhost:27017/blogDB';
 
+const options = {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    serverSelectionTimeoutMS: 5000,
+    autoIndex: false, // Don't build indexes
+    maxPoolSize: 10, // Maintain up to 10 socket connections
+    serverSelectionTimeoutMS: 5000, // Keep trying to send operations for 5 seconds
+    socketTimeoutMS: 45000, // Close sockets after 45 seconds of inactivity
+    family: 4 // Use IPv4, skip trying IPv6
+}
+
+mongoose.connect(uri, options);
+
+const blogSchema = {
+  title: String,
+  body: String
+}
+
+const Blog = mongoose.model("Blog", blogSchema);
+
+const blog = new Blog({
+  title: "mongoDB Blog!!",
+  body: "This is my first mongoDB blog"
+});
+
+// blog.save();
 
 app.get('/', function(req, res) {
-  res.render("home", {homeContent: homeStartingContent, posts:posts});
+  Blog.find(function(err, posts){
+    res.render("home", {posts:posts});
+  });
+
 
 });
 
 app.get('/posts/:postName', function(req, res) {
 
   let requestedTitle = _.lowerCase(req.params.postName);
+  Blog.find(function(err, posts){
+    posts.forEach(function(post){
 
-  posts.forEach(function(post){
-
-    if(requestedTitle===_.lowerCase((post.title))){
-      res.render("post",{postTitle: post.title, postContent: post.content});
-    }
+      if(requestedTitle===_.lowerCase((post.title))){
+        res.render("post",{postTitle: post.title, postContent: post.body});
+      }
+    });
   });
+
 
 
 });
@@ -51,11 +82,24 @@ app.get("/compose", function(req, res) {
 
 app.post('/compose', function(req, res) {
 
-  const post = {
+  const post = new Blog({
     title: req.body.postTitle,
-    content: req.body.postBody
-  };
-  posts.push(post);
+    body: req.body.postBody
+  });
+
+  Blog.findOne({title: req.body.postTitle}, function(err, found){
+    postFound = "";
+    if(!err){
+      if(!found){
+        post.save();
+      }
+      else{
+        console.log("Post with same title already exists");
+          res.redirect("/");
+      }
+    }
+  })
+  // posts.push(post);
   res.redirect("/");
 
 });
